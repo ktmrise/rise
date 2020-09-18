fd3f6fa1c625fb988f143836f772526fd47df804
ktmrise <2413114403@qq.com>
2020-09-18 18:11:29 +0800

    index 分页

--- src/main/java/com/ktm/controller/IndexController.java
+++ src/main/java/com/ktm/controller/IndexController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.annotation.Resource;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -30,8 +32,8 @@ public class IndexController {
 
     @GetMapping({"/index", "/"})
     public String index(HttpServletRequest request,
-                        @RequestParam(name = "current",defaultValue = "1") int current,
-                        @RequestParam(name = "size",defaultValue = "4") int size) {
+                        @RequestParam(name = "current", defaultValue = "1") int current,
+                        @RequestParam(name = "size", defaultValue = "2", required = false) int size) {
         Cookie[] cookies = request.getCookies();
         for (Cookie cookie : cookies) {
             if (cookie.getName().equals("token")) {
@@ -45,9 +47,64 @@ public class IndexController {
 
 
         IPage<QuestionDTO> result = questionService.paging(new Page<QuestionDTO>(current, size));
-        request.setAttribute("questions",result.getRecords());
+
+        //返回要显示的页码
+        List<Integer> pages = selectPages(current, result.getPages());
+
+        //是否包含第一页
+        boolean isContainFirst = isContainFirst(pages);
+        boolean isContainEnd = isContainEnd(pages, (int) result.getPages());
+
+        request.setAttribute("questions", result.getRecords());
+        request.setAttribute("pages", pages);
+        request.setAttribute("total", result.getPages());
+        request.setAttribute("current", current);
+        request.setAttribute("isContainFirst",isContainFirst);
+        request.setAttribute("isContainEnd",isContainEnd);
         return "index";
     }
+
+    private boolean isContainEnd(List<Integer> pages, int resultPages) {
+        if (pages.contains(resultPages)) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isContainFirst(List<Integer> pages) {
+        if (pages.contains(1)) {
+            return true;
+        }
+        return false;
+    }
+
+    private List<Integer> selectPages(int current, long totalPages) {
+        List<Integer> pageList = new ArrayList<>();
+        if (current <= 3) {
+            for (int i = 1; i <= 3 + current; i++) {
+                pageList.add(i);
+            }
+            return pageList;
+        }
+
+        if (current < totalPages - 3) {
+            for (int i = current - 3; i <= current + 3; i++) {
+                pageList.add(i);
+            }
+            return pageList;
+        }
+
+        if (current >= totalPages - 6) {
+            for (int i = (int) (totalPages - 6); i <= totalPages; i++) {
+                pageList.add(i);
+            }
+            return pageList;
+        }
+
+        return pageList;
+
+    }
+
 
     @RequestMapping("/logout")
     public String logout() {
