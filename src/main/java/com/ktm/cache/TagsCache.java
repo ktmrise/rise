065446811483c489737eb965f8e3ba0362e2ee50
ktmrise <2413114403@qq.com>
2020-09-27 13:24:17 +0800

    tag 换行

--- src/main/java/com/ktm/cache/TagsCache.java
+++ src/main/java/com/ktm/cache/TagsCache.java
@@ -9,32 +9,67 @@ import java.util.stream.Collectors;
 
 public class TagsCache {
 
+    static List<TagDTO> tags;
+
+
+    static class InnerTag {
+
+        static {
+            TagDTO lang = new TagDTO();
+            lang.setCategoryName("开发语言");
+            lang.setTagNames(Arrays.asList("Java", "python", "c", "golang", "c++", "c#", "javascript", "php", "perl", "ruby", "lua", "node.js", "erlang", "scala", "bash", "actionscript", "typescript"));
+
+            TagDTO framework = new TagDTO();
+            framework.setCategoryName("平台框架");
+            framework.setTagNames(Arrays.asList("Spring", "Spring Boot", "Spring Mvc", "mybatis", "jpa", "Spring Cloud","hibernate"));
+
+
+            TagDTO server = new TagDTO();
+            server.setCategoryName("服务器");
+            server.setTagNames(Arrays.asList("nginx", "tomcat", "linux", " apache"));
+
+
+            TagDTO database = new TagDTO();
+            database.setCategoryName("数据库");
+            database.setTagNames(Arrays.asList("mysql", "oracle", "h2", "redis", "mongodb","elasticsearch","memcached"));
+
+            TagDTO developKit = new TagDTO();
+            developKit.setCategoryName("开发工具");
+            developKit.setTagNames(Arrays.asList(
+                    "eclipse"
+                    , "vs code"
+                    , "maven"
+                    , "gradle"
+                    , "webstorm"
+                    , "ide "
+                    , "eclipse"
+                    , "xcode"
+                    , "intellij-idea"
+                    , "macosx"
+                    , "bundle"
+                    , "vim"
+                    , "emacs"
+                    , "intellij-idea"
+                    , "textmate"
+                    , "sublime-text"
+                    , "visual-studio"
+                    , "git"
+                    , "github"
+                    , "svn"
+                    , "hg"
+                    , "docker ci"));
+            tags = Arrays.asList(lang, framework, server, database, developKit);
+        }
+
+        public static List<TagDTO> initTagDTO() {
+            return tags;
+        }
+
+    }
+
+
     public static List<TagDTO> getTagDTO() {
-        TagDTO lang = new TagDTO();
-        lang.setCategoryName("开发语言");
-        lang.setTagNames(Arrays.asList("Java", "python","c","golang","c++","shell","c#","javascript","php"));
-
-        TagDTO framework = new TagDTO();
-        framework.setCategoryName("平台框架");
-        framework.setTagNames(Arrays.asList("Spring", "Spring Boot","Spring Mvc","mybatis","jpa"));
-
-
-        TagDTO server = new TagDTO();
-        server.setCategoryName("服务器");
-        server.setTagNames(Arrays.asList("nginx", "tomcat","linux"));
-
-
-        TagDTO database = new TagDTO();
-        database.setCategoryName("数据库");
-        database.setTagNames(Arrays.asList("mysql", "oracle", "h2", "redis", "mongdb"));
-
-        TagDTO developKit = new TagDTO();
-        developKit.setCategoryName("开发工具");
-        developKit.setTagNames(Arrays.asList("idea","eclipse","vs code","maven","gradle","webstorm"));
-
-
-        return Arrays.asList(lang, framework,server,database,developKit);
-
+        return InnerTag.initTagDTO();
     }
 
 
@@ -48,3 +83,6 @@ public class TagsCache {
     }
 
 }
+
+
+
